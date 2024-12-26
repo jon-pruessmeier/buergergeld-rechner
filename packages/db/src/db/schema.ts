f6e2aa0a27e1cfd8944d9c42a3e5036fb5ed92b0
Jón Prüßmeier <pruessmeierjon@gmail.com>
2024-12-26 12:39:45 +0100

    init drizzle #3

---
+++ packages/db/src/db/schema.ts
@@ -0,0 +1,12 @@
+import { numeric, pgTable, uuid, varchar } from "drizzle-orm/pg-core";
+
+export const citizenTable = pgTable("citizen", {
+  id: uuid().notNull(),
+  name: varchar({ length: 255 }).notNull(),
+  //   surname: varchar({ length: 255 }).notNull(),
+  //   housingType: varchar({ enum: ["RENT", "OWNERSHIP"] }),
+  //   loanInterestMonth: numeric(),
+  //   coldRentMonth: numeric(),
+  //   additionalCostsMonth: numeric(),
+  //   heatingCostsMonth: numeric(),
+});
